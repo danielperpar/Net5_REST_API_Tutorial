fef71910a82e76f14f04e0ffae2d9d511b676f57
Daniel Pérez <daniel.perezpa@hotmail.es>
2022-06-07 17:40:20 +0200

    adding crud with inmemory data

---
+++ Entities/Item.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Dtos;
+using System;
+
+namespace Catalog.Entities
+{
+    public record Item
+    {
+        public Guid Id { get; init; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
